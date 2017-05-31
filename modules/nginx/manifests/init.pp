af3587d88a2da005676c809eaee736ed7aa1fe69
nelson <nelson@puppetlabs.vm>
2017-05-31 18:22:20 +0000

    Adicionando default para evitar repeticao de codigo

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -4,26 +4,24 @@ package { 'nginx' :
   ensure => present,
 }
 
+File {
+  owner => 'root',
+  group => 'root',
+  mode  => '0664',
+
+}
+
 file { '/var/www' :
   ensure => directory,
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0775',
 }
 
 file { '/var/www/index.html' :
   ensure => file,
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0664',
   source => 'puppet:///modules/nginx/index.html',
 }
 
 file { '/etc/nginx/nginx.conf' :
   ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0664',
   source  => 'puppet:///modules/nginx/nginx.conf',
   require => Package['nginx'],
   notify  => Service['nginx'],
@@ -31,9 +29,6 @@ file { '/etc/nginx/nginx.conf' :
 
 file { '/etc/nginx/conf.d/default.conf' :
   ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0664',
   source  => 'puppet:///modules/nginx/default.conf',
   require => Package['nginx'],
   notify  => Service['nginx'],
